47aeac8fff7637d3571c3e8541d2749caecda3d5
Zoltán Adamek <djscorchio@gmail.com>
2020-09-21 20:39:35 +0200

    Remove wrong stuff from 145

--- 145/high_low_temps.py
+++ 145/high_low_temps.py
@@ -9,14 +9,4 @@ STATION = namedtuple("Station", "ID Date Value")
 
 
 def high_low_record_breakers_for_2015():
-    def _date_to_monthday(value):
-        d = date.fromisoformat(value)
-        return f'{d.month}-{d.day}'
-
-    df = pd.read_csv(DATA_FILE)
-    df['monthday'] = df['Date'].apply(_date_to_monthday)
-    df['min'] = df.groupby(['ID', 'monthday'])['Data_Value'].min()
-    df['max'] = df.groupby(['ID', 'monthday'])['Data_Value'].max()
     pass
-
-high_low_record_breakers_for_2015()
