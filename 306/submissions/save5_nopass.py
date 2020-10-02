051d57168ec0ce1e21790d4b2e131c4992feda84
Zoltán Adamek <zoltan.adamek@prezi.com>
2020-10-02 18:56:49 +0200

    Solve 306

---
+++ 306/submissions/save5_nopass.py
@@ -0,0 +1,14 @@
+import string
+
+from Bio.Seq import Seq
+
+
+def translate_cds(cds: str, translation_table: str) -> str:
+    """
+    :param cds: str: DNA coding sequence (CDS)
+    :param translation_table: str: translation table as defined in Bio.Seq.Seq.CodonTable.ambiguous_generic_by_name
+    :return: str: Protein sequence
+    """
+    cds_cleaned = cds.encode('ascii', 'ignore').decode().translate(str.maketrans('', '', string.whitespace)).upper()
+    seq = Seq(cds_cleaned)
+    return seq.translate(table=translation_table)
